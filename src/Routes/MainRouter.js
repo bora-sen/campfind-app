025debddaa7730c64dd9581be7713ce6ff57b3e9
bora-sen <bahattinborasen@gmail.com>
2023-03-20 00:21:20 +0300

    +updated Routes

--- src/Routes/MainRouter.js
+++ src/Routes/MainRouter.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { Routes,Route } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Routes,Route, Navigate } from 'react-router-dom'
+import { authContext } from '../Context/auth'
 import * as Pages from '../Pages'
+import UserSpesificPage from './UserSpesificPage'
 
 
 function MainRouter() {
+  const {user} = useContext(authContext);
   return (
     <Routes>
-        <Route path='/' element={<Pages.Landing />} />
+        <Route path='/' element={
+          <UserSpesificPage user={user} isAuthEl={<Navigate to="/camps" />} isNotAuthEl={<Pages.Landing />} />
+        } />
+
         <Route path='/sign-in' element={<Pages.SignIn />} />
         <Route path='/sign-up' element={<Pages.SignUp />} />
         <Route path='/camps' element={<Pages.Explore />} />
         <Route path='/camp/:id' element={<Pages.Campground />} />
+        <Route path='/create/campground' element={<Pages.AddCampground />} />
     </Routes>
   )
 }
---
+++ src/Routes/UserSpesificPage.js
@@ -0,0 +1,10 @@
+function UserSpesificPage({user,isAuthEl,isNotAuthEl}) {
+  if(user){
+    return isAuthEl
+  }
+  else {
+    return isNotAuthEl
+  }
+}
+
+export default UserSpesificPage
