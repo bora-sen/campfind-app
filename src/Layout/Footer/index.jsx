1abf681eddf2fcbecd7aee7b48a1ce5aa07010b8
bora-sen <bahattinborasen@gmail.com>
2023-03-21 00:21:24 +0300

    +update: Layout Components

--- src/Layout/Footer/index.jsx
+++ src/Layout/Footer/index.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import * as Assets from '../../Pages/Landing/Assets'
 
 function Footer() {
-  return (
-    <nav className='flex flex-col justify-between p-8'>
-      <div className='flex items-center'>
-      <NavLink replace to="/"><h1 className='font-bold text-3xl'>campFind</h1></NavLink>
+  return (<>
+    <nav className='flex flex-col md:flex-row justify-between items-center p-8 bg-primary-accent text-white'>
+      <div className='mb-4 md:w-1/4'>
+        <NavLink replace to="/"><h1 className='font-bold text-4xl'>campFind</h1></NavLink>
       </div>
-      <div className='text-center text-xs'>
-        <span>Copyright 2023 | campFind Made By. <a className='font-semibold' href="https://bborasen.com">Bora</a></span>
-      </div>
+
+        <ul className='flex flex-col md:w-3/4 sm:flex-row items-center justify-end sm:mr-12 gap-24 my-24 sm:my-4'>
+          <li>
+            <NavLink replace to="">Sign In</NavLink>
+          </li>
+          <li>
+            <NavLink replace to="">Create an account</NavLink>
+          </li>
+          <li>
+            <NavLink replace to="">Explore</NavLink>
+          </li>
+          <li>
+            <NavLink replace to="">Login</NavLink>
+          </li>
+        </ul>
     </nav>
+    <div className='text-center text-xs bg-primary-accent-darker text-white'>
+      <span>Copyright 2023 | campFind Made By. <a className='font-semibold' href="https://bborasen.com">Bora</a></span>
+    </div>
+    </>
   )
 }
 
--- src/Layout/Header/index.jsx
+++ src/Layout/Header/index.jsx
@@ -23,19 +23,19 @@ function Header() {
   return (
     <nav className='flex justify-between p-8'>
       <div className='flex items-center'>
-        <NavLink replace to="/"><h1 className='font-bold text-5xl'>campFind</h1></NavLink>
+        <NavLink replace to="/"><h1 className='font-bold text-4xl text-primary-accent'>campFind</h1></NavLink>
         <span className='hidden font-bold text-gray-400'>Home</span>
       </div>
       <div className='md:hidden'>
         <button className='bg-gray-100 p-2 rounded-md'>
-        <svg width={25} height={25} fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M3 12h18" /><path d="M3 6h18" /><path d="M3 18h18" /></svg>
+        <svg width={25} height={25} fill="none" stroke="#06140D" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M3 12h18" /><path d="M3 6h18" /><path d="M3 18h18" /></svg>
         </button>
       </div>
       <div className='hidden md:flex md:items-center md:gap-8'>
         {
           !user ? <>
             <NavLink replace to='/sign-in' className='font-bold text-gray-400'>Login</NavLink>
-            <NavLink replace to="/sign-up" className='font-bold text-white bg-gray-900 p-4'>Create an account</NavLink>
+            <NavLink replace to="/sign-up" className='font-bold text-white bg-primary-black p-4'>Create an account</NavLink>
           </>
           :
           <>
