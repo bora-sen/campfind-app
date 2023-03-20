985ebd27f386fdb84e1d721c8bd0eeb04a55b21c
bora-sen <bahattinborasen@gmail.com>
2023-03-21 00:22:56 +0300

    -del : Custom Hook

--- src/Hooks/useLocalStorage.js
+++
@@ -1,7 +0,0 @@
-export function useLocalStorage(){
-    const getFromLocalStorage = JSON.parse(localStorage.getItem('user')) ?? false;
-    function saveToLocalStorage(val){
-        localStorage.setItem('user',JSON.stringify(val));
-    }
-    return [getFromLocalStorage,saveToLocalStorage]
-}
