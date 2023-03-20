71454c87332b0656bdd2070d1182f8cf79d36003
bora-sen <bahattinborasen@gmail.com>
2023-03-21 00:23:09 +0300

    +added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary-white":"#FFF",
+        "primary-black":"#06140D",
+        "primary-accent":"#2db148",
+        "primary-accent-darker":"#006B32",
+        "primary-accent-pale":"#46EB93",
+        "primary-danger":"#DD2E00"
+      }
+    },
   },
   plugins: [],
 }
