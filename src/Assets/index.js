1f457d57c237312eaf9802304d830f374fa13e3c
bora-sen <bahattinborasen@gmail.com>
2023-03-27 14:46:13 +0300

    +added : Assets

---
+++ src/Assets/index.js
@@ -0,0 +1,7 @@
+export { default as AirbnbLogo } from "./airbnb.svg";
+export { default as BookingLogo } from "./booking.svg";
+export { default as Checkmark } from "./checkmark.svg";
+export { default as ExplorePlaceholder } from "./explore-placeholder.jpg";
+export { default as HeroImgDesktop } from "./hero-image-desktop.jpg";
+export { default as HeroImgMobile } from "./hero-image-mobile.jpg";
+export { default as PlumGuideLogo } from "./plum-guide.svg";
