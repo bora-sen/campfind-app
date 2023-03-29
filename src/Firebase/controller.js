431e96ef645cf2b7e31b9127f3478e097aeff5a7
bora-sen <bahattinborasen@gmail.com>
2023-03-29 18:34:26 +0300

    +update firebase controller

--- src/Firebase/controller.js
+++ src/Firebase/controller.js
@@ -1,50 +1,36 @@
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  updateProfile,
-} from "firebase/auth";
-import {
-  addDoc,
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  query,
-  setDoc,
-  updateDoc,
-} from "firebase/firestore";
-import { uuidv4 } from "@firebase/util";
+import { uuidv4 } from "@firebase/util"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth"
+import { collection, doc, getDoc, getDocs, query, setDoc, updateDoc } from "firebase/firestore"
 
-import { auth, db } from "./index";
+import { auth, db } from "./index"
 
-const campgroundsRef = collection(db, "campgrounds");
+const campgroundsRef = collection(db, "campgrounds")
 
 export async function Login(email, password) {
   try {
-    const resp = await signInWithEmailAndPassword(auth, email, password);
-    return resp;
+    const resp = await signInWithEmailAndPassword(auth, email, password)
+    return resp
   } catch (error) {
-    console.log(error);
-    return null;
+    console.log(error)
+    return null
   }
 }
 export async function SignUp(name, email, password) {
-  const resp = await createUserWithEmailAndPassword(auth, email, password);
-  await updateProfile(auth.currentUser, { displayName: name });
-  return resp.user;
+  const resp = await createUserWithEmailAndPassword(auth, email, password)
+  await updateProfile(auth.currentUser, { displayName: name })
+  return resp.user
 }
 export async function SignOut() {
   try {
-    await signOut(auth);
-    console.log("LOGOUT");
+    await signOut(auth)
+    console.log("LOGOUT")
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 export async function createCampground(user, campgroundObj) {
   try {
-    const newId = uuidv4();
+    const newId = uuidv4()
     const newCampground = {
       _id: newId,
       title: campgroundObj.title,
@@ -54,14 +40,11 @@ export async function createCampground(user, campgroundObj) {
       userId: user.uid,
       userName: user.displayName,
       comments: [],
-    };
-    let resp = await setDoc(
-      doc(db, "campgrounds", newCampground._id.toString()),
-      newCampground
-    );
-    console.log(resp);
+    }
+    let resp = await setDoc(doc(db, "campgrounds", newCampground._id.toString()), newCampground)
+    console.log(resp)
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
@@ -76,57 +59,77 @@ createCampground({uid:"alsdjfklsjdf",email:"bora@test.com",name:"Bora"},{
 
 export async function getAllCampgrounds() {
   try {
-    const q = query(campgroundsRef, {});
-    const camps = [];
-    const requestSnapshot = await getDocs(q);
+    const q = query(campgroundsRef, {})
+    const camps = []
+    const requestSnapshot = await getDocs(q)
     requestSnapshot.forEach((camp) => {
-      camps.push(camp.data());
-    });
-    return camps;
+      camps.push(camp.data())
+    })
+    return camps
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 export async function getCampgroundById(campgroundId) {
   try {
-    const q = query(doc(db, "campgrounds", campgroundId.toString()));
-    const resp = await getDoc(q);
-    const camp = resp.data();
-    return resp.data();
+    const q = query(doc(db, "campgrounds", campgroundId.toString()))
+    const resp = await getDoc(q)
+    const camp = resp.data()
+    return resp.data()
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 export async function getComments(campgroundId) {
   try {
-    const sel_camp = (await getCampgroundById(campgroundId)) ?? false;
+    const sel_camp = (await getCampgroundById(campgroundId)) ?? false
     if (!sel_camp) {
-      console.log("There is no campground, returning..");
-      return;
+      console.log("There is no campground, returning..")
+      return
     }
-    return sel_camp.comments;
+    return sel_camp.comments
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
 export async function addCommentToCampground(campgroundId, commentObj) {
   try {
-    const newUUID = uuidv4();
-    const newCampObj = await getCampgroundById(campgroundId);
+    const newUUID = uuidv4()
+    const newCampObj = await getCampgroundById(campgroundId)
 
     const newComment = {
       _id: newUUID,
       author: commentObj.author,
       content: commentObj.content,
-    };
+    }
 
-    newCampObj.comments.push(newComment);
+    newCampObj.comments.push(newComment)
 
-    await updateDoc(doc(db, "campgrounds", campgroundId), newCampObj);
+    await updateDoc(doc(db, "campgrounds", campgroundId), newCampObj)
 
-    console.log("Comment is Added!!!");
+    console.log("Comment is Added!!!")
   } catch (error) {
-    console.log(error);
+    console.log(error)
+  }
+}
+
+export async function removeCommentFromCampground(campgroundId, commentId) {
+  try {
+
+    //console.log(campgroundId,commentId);
+    const sel_camp = await getCampgroundById(campgroundId)
+    console.log(sel_camp);
+
+    sel_camp.comments = sel_camp.comments.filter(comment => {
+      return comment._id !== commentId
+    })
+
+    await updateDoc(doc(db, "campgrounds", campgroundId), sel_camp)
+    console.log("Comment id of -> ", commentId, " is deleted!!")
+    console.log(sel_camp.comments)
+    
+  } catch (error) {
+    console.log(error)
   }
 }
